338f9f2b7d1cb39ad9d8fbe5622c959da8961613
Ona <onushkis@gmail.com>
2021-12-28 15:22:46 +0100

    add Navigation DONE

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import '../styles/menu.scss';
+import { ReactComponent as Home} from '../images/home.svg';
+import { ReactComponent as Inbox} from '../images/inbox.svg';
+import { ReactComponent as NewPost} from '../images/newPost.svg';
+import { ReactComponent as Explore} from '../images/explore.svg';
+import { ReactComponent as Notifications} from '../images/notifications.svg';
+import image from '../images/profil.jpg';
+import ProfileIcon from './ProfileIcon'
+
+const Menu = () => {
+    return ( 
+<div className="menu">
+<Home className='icon' />
+<Inbox className='icon' />
+<NewPost className='icon' />
+<Explore className='icon' />
+<Notifications className='icon' />
+<ProfileIcon iconSize="small" image={image}/>
+
+</div>
+     );
+}
+ 
+export default Menu;
